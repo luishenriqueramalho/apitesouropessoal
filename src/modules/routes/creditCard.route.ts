36f8290bff1811a458bebd10d6d6ac790f07722b
luishenriqueramalho <luishenriqueramalho.ramalhodasilva@gmail.com>
2023-10-06 08:03:40 -0300

    rotas com auth

--- src/modules/routes/creditCard.route.ts
+++ src/modules/routes/creditCard.route.ts
@@ -4,11 +4,12 @@ import {
   deleteCreditCardHandler,
   getCreditCardHandler,
 } from "../controllers/creditCard.controller";
+import { authRoutes } from "../middleware/auth.middleware";
 
 async function creditCardRoutes(app: FastifyInstance) {
-  app.get("/api/creditCard", getCreditCardHandler);
-  app.post("/api/creditCard", createCreditCardHandler);
-  app.delete("/api/creditCard/:id", deleteCreditCardHandler);
+  app.get("/api/creditCard", authRoutes, getCreditCardHandler);
+  app.post("/api/creditCard", authRoutes, createCreditCardHandler);
+  app.delete("/api/creditCard/:id", authRoutes, deleteCreditCardHandler);
 }
 
 export default creditCardRoutes;
