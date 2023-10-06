8b96aebae710290aae620a8813523d6a353c7926
luishenriqueramalho <luishenriqueramalho.ramalhodasilva@gmail.com>
2023-10-06 15:49:34 -0300

    auth

--- src/modules/routes/user.route.ts
+++ src/modules/routes/user.route.ts
@@ -10,14 +10,14 @@ import {
 import { authRoutes } from "../middleware/auth.middleware";
 
 async function userRoutes(app: FastifyInstance) {
-  app.get("/api/user", getUserHandler);
-  app.get("/api/findUser/:id", getUserByIdHandler);
+  app.get("/api/user", authRoutes, getUserHandler);
+  app.get("/api/findUser/:id", authRoutes, getUserByIdHandler);
 
   app.post("/api/login", loginHandler);
-  app.post("/api/logout", logoutHandler);
+  app.post("/api/logout", logoutHandler); // TODO
   app.post("/api/user", createUserHandler);
 
-  app.delete("/api/user/:id", deleteUserHandler);
+  app.delete("/api/user/:id", authRoutes, deleteUserHandler);
 }
 
 export default userRoutes;
